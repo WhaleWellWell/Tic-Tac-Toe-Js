cc277208ff6d39ecfec614deaf9a5e16fcc93a26
Whale Well Well <AsherEverettDev@gmail.com>
2022-05-23 16:20:02 -0400

    Added turn handler

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,43 @@
+const X = "X";
+const O = "O";
+let turn = X; // Player that starts the game
+
+const subheader = document.getElementsByClassName("subheader");
+
+
+// Button Click Handlers:
 
 //! There is most likely a better way to do this.
-const allButtons = document.querySelectorAll(".btn");
+const btn1 = document.getElementsByClassName("btn-1")
+const btn2 = document.getElementsByClassName("btn-2")
+const btn3 = document.getElementsByClassName("btn-3")
+const btn4 = document.getElementsByClassName("btn-4")
+const btn5 = document.getElementsByClassName("btn-5")
+const btn6 = document.getElementsByClassName("btn-6")
+const btn7 = document.getElementsByClassName("btn-7")
+const btn8 = document.getElementsByClassName("btn-8")
+const btn9 = document.getElementsByClassName("btn-9")
+
+const allButtons = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9]
 
 allButtons.forEach(element => {
-  element.addEventListener('click', handleBtnClick); // <=== would like this to be: handleBtnClick(element)
+  element.addEventListener('click', handleBtnClick); 
   // I would like to add here a way to specify witch btn is being clicked.
 
   // eg: First button is clicked:
-  // Function called: handleBtnClick("btn1") or handleBtnClick(1)
+  // Function called: handleBtnClick(someWayToDescribeButton)
 });
 
-
-
-
-
-
 function handleBtnClick (btn) {
-  alert("test click");
+  if (btn.innerHTML === "") { // There is nothing there if the button is empty
+    btn.innerHTML = turn;
+  } else {
+    // TODO: Make some way to tell the player that they messed up
+  };
+
+  if (turn === X) {
+    turn = O;
+  } else {
+    turn = X;
+  };
 }
